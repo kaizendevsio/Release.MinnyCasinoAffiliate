19538ccf22968d18f371a8b74781eebea42f6ddc
kaizendevsio <jjeeraldo@gmail.com>
2019-11-30 20:49:26 +0800

    test 3

--- MinnyCasinoAffiliate.FrontEnd.Published/wwwroot/backend/app.js
+++
@@ -1,9 +0,0 @@
-$(".passtoggle").click(function () {
-    var passwordInput = $("#password");
-    $(this).toggleClass("uil-eye uil-eye-slash");
-    if (passwordInput.attr("type") == "password") {
-        passwordInput.attr("type", "text");
-    } else {
-        passwordInput.attr("type", "password");
-    }
-});
